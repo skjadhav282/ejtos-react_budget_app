d77b1d87e11935a51890c24a8bb0ec2a171979a6
skjadhav282 <skjadhav282@gmail.com>
2023-09-03 13:39:46 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,11 +4,15 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 //Code to import Budget.js
 import Budget from './components/Budget';
-
+import ExpenseItem from './components/ExpenseItem';
+import ExpenseTotal from './components/ExpenseTotal';
+import Remaining from './components/Remaining';
 // Add code to import the other components here under
 
 
 import { AppProvider } from './context/AppContext';
+import ExpenseList from './components/ExpenseList';
+import AllocationForm from './components/AllocationForm';
 const App = () => {
     return (
         <AppProvider>
@@ -16,17 +20,29 @@ const App = () => {
                 <h1 className='mt-3'>Company's Budget Allocation</h1>
                     <div className='row mt-3'>
                         {/* Add Budget component here under */}        
-
+                    <div className='col-sm'>
+                        <Budget />
+                    </div>
                         {/* Add Remaining component here under */}        
-
+                    <div className='col-sm'>
+                        <Remaining />
+                    </div>
                         {/* Add ExpenseTotal component here under */}        
-                       
+                    <div className='col-sm'>
+                        <ExpenseTotal />
+                    </div>
                         {/* Add ExpenseList component here under */}         
-
+                    <div className='col-sm'>
+                        <ExpenseList />
+                    </div>
                         {/* Add ExpenseItem component here under */}        
-
+                    <div className='col-sm'>
+                        <ExpenseItem />
+                    </div>
                         {/* Add AllocationForm component here under */}        
-
+                    <div className='col-sm'>
+                        <AllocationForm />
+                    </div>
                 </div>
             </div>
         </AppProvider>
